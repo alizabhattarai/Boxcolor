ba3ff9a22371a05d1050d49a7ed8f17d4125c989
alizabhattarai <alizabi2000@gmail.com>
2020-07-31 16:11:10 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
+// first defin the Div class name
+//create another div with in a div with id = "blue"
+//give  a style and size to a div with id="blue"
+
+
+
+
+
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return(
+      <div className= "App">
+        <div id= "blue"></div>
+
+      
+      </div>
+    )
+  }
 }
 
 export default App;
