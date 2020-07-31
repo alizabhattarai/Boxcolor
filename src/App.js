bb3617f5bec81cccc493296787715fc4e56911e7
alizabhattarai <alizabi2000@gmail.com>
2020-07-31 17:48:50 -0500

    forth commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // first update the functional component to class component 
 //set 2 state color and clickCounter
 //write funciton to show increase in click counter
+//write a function to toggle click
 //write a function to show change in color of div 
 // 
 
@@ -13,25 +14,41 @@ import './App.css';
 class App extends React.Component {
   constructor (props) {
     super(props);
-      this. state ={
-      color:'',
-      clickCounter:'0',
+      this.state ={
+      color:'true',
+      clicks:'0',
     };
   }
 
+clickcounter = () => {
+  this.setState( {clicks: this.state.clicks +1});
 
   }
+
+ToggleClick = () => {
+  this.setState({show : !this.state.show });
+}
+
+changecolor = () => {
+  let newColor = this.state.color =='orange';
+  this.setState(
+    {color: newColor});
+}
+
     render (){
       return (
         <div>
-          <div className="box">
+          <div className="Box">
+          onClick ={this.clickcounter}
+          onClick ={this.ToggleClick}
+          onClick={this.changecolor}
           </div>
         </div>
       );
 
 
   }
-
+}
 
 
 export default App;
